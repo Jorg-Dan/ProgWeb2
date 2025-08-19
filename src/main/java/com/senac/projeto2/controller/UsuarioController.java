965993abdc73d2daf01f0fbb8c7e03869621725d
JD <jorgedaniel.fcamelo@gmail.com>
2025-08-18 21:40:16 -0300

    inclusão do service e atualização do repository

--- src/main/java/com/senac/projeto2/controller/UsuarioController.java
+++ src/main/java/com/senac/projeto2/controller/UsuarioController.java
@@ -1,34 +1,49 @@
 package com.senac.projeto2.controller;
 
+import com.senac.projeto2.entity.Usuario;
+import com.senac.projeto2.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/usuario")
 @Tag(name="Usuario", description="api para gerenciamento dos usuários do sistema")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/listar")
     @Operation(summary = "Listar usuarios do sistema")
-    public String listar(){
-        return "Listando com sucesso";
+    public ResponseEntity<List<Usuario>> listar(){
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
     }
+
     @GetMapping("/listarPoridUsuario/{idUsuario}")
     @Operation (summary = "Listar usuarios do sistema pelo id do usuário")
-    public String listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
-        return "Listando um usuario por id com sucesso"+ idUsuario + " com sucesso";
+    public ResponseEntity<Usuario> listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
+        return ResponseEntity.ok(usuarioService.listarUsuarioPorId(idUsuario));
     }
 
     @PostMapping("/criar")
     public String criar(){
         return "Usuário criado com sucesso!";
     }
+
     @PutMapping("/atualizar")
     public String atualizar(){
         return "Usuário atualizado com sucesso";
     }
+
     @DeleteMapping("/apagar")
     public String apagar(){
         return "Usuário apagado com sucesso";
     }
+
 }

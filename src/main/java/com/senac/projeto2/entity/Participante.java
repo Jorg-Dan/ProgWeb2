26baaeea560fe74289fb5431558919bf4c95c9a4
JD <jorgedaniel.fcamelo@gmail.com>
2025-08-21 21:37:58 -0300

    Criação atualização do participante e criação do seu repositório

--- src/main/java/com/senac/projeto2/entity/Participante.java
+++ src/main/java/com/senac/projeto2/entity/Participante.java
@@ -1,4 +1,74 @@
 package com.senac.projeto2.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="participante")
 public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participante_id")
+    private int id;
+    @Column(name = "participante_nome", nullable = false, length = 300)
+    private String nome;
+    @Column(name = "participante_email", nullable = false, length = 45)
+    private String email;
+    @Column(name = "participante_identificacao", nullable = false, length = 45)
+    private String identificacao;
+    @Column(name = "participante_endereco", nullable = false, length = 300)
+    private LocalDate endereco;
+    @Column(name = "participante_status", nullable = false)
+    private int status;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(String identificacao) {
+        this.identificacao = identificacao;
+    }
+
+    public LocalDate getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(LocalDate endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/senac/projeto2/entity/Usuario.java
+++ src/main/java/com/senac/projeto2/entity/Usuario.java
@@ -1,7 +1,6 @@
 package com.senac.projeto2.entity;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
 @Entity

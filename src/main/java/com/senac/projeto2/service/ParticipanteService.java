01a06797a405bebef78f6070dcd65706a5d2ddce
JD <jorgedaniel.fcamelo@gmail.com>
2025-08-21 21:41:35 -0300

    Atualização do participante service

--- src/main/java/com/senac/projeto2/service/ParticipanteService.java
+++ src/main/java/com/senac/projeto2/service/ParticipanteService.java
@@ -1,4 +1,12 @@
 package com.senac.projeto2.service;
 
+import com.senac.projeto2.entity.Participante;
+import com.senac.projeto2.repository.ParticipanteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
 public class ParticipanteService {
+
 }

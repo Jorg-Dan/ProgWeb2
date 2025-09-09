3dca29090097e51307819df49f64916f0c7c7c4a
JD <jorgedaniel.fcamelo@gmail.com>
2025-09-08 21:35:10 -0300

    Atualização do pom e dos usuarios, criação dos validators

--- src/main/java/com/senac/projeto2/repository/ParticipanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senac.projeto2.repository;
-
-import com.senac.projeto2.entity.Participante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParticipanteRepository extends JpaRepository<Participante, Integer> {
-}
